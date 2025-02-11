649e39237fe699a52d0d938c1ba84122722702bd
Mohammad Hashir Siddiqui <113341675+Hashir311@users.noreply.github.com>
2025-02-11 09:44:40 +0530

    setup server and endpoints

---
+++ backend/server/apps/endpoints/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class Endpoint(models.Model):
+    class Meta:
+        app_label = 'endpoints'
+    name = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+class MLAlgorithm(models.Model):
+    class Meta:
+        app_label = 'mlalgorithms'
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+
+class MLAlgorithmStatus(models.Model):
+    class Meta:
+        app_label = 'mlalgorithmstatuses'
+    status = models.CharField(max_length=128)
+    active = models.BooleanField()
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name = "status")
+
+class MLRequest(models.Model):
+    class Meta:
+        app_label = 'mlrequests'
+    input_data = models.CharField(max_length=10000)
+    full_response = models.CharField(max_length=10000)
+    response = models.CharField(max_length=10000)
+    feedback = models.CharField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
